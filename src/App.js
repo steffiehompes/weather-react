885d245dd0a0c82847c3c0fc54d2d7c85140cc07
steffiehompes <82502716+steffiehompes@users.noreply.github.com>
2021-06-12 22:01:24 +0200

    Created Weather component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Testing </h1>
+        <Weather />
       </header>
     </div>
   );
